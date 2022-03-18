1f2a6480807fa5edebed477b75fc886e55cc1c30
Zo Andrianambinina <zo@livenexx.net>
2022-03-18 09:34:08 +0300

    2 lot avec fonction aléatoire

--- quiz.py
+++ quiz.py
@@ -1,47 +1,59 @@
 # Importation des bibliothèques
 import csv
+import random
 
 score = 0
 count = 0
 
-options = {
-    'A': 1,
-    'B': 2,
-    'C': 3,
-    'D': 4
-}
+options = {'A': 1, 'B': 2, 'C': 3, 'D': 4}
 
 # Vérification des réponses
 def checkResponse(response, answer):
     global score, applied_ques
     if response == answer:
-        print('Bonne réponse!')
+        print('Bonne réponse!\n')
         score += 2
     else:
-        print(f'Faux !, la réponse est: {answer}')
+        print(f'Faux !, la réponse est: {answer}\n')
 
 # Affichage du score
 def showScore():
     print(f'Score total: {score}/20.')
+    
+# Export des questions
+def exportCSV(file):
+    with open(file, 'r') as file:
+        reader = csv.reader(file)
+        response = list(reader)
+        response.pop(0)
+        return response
+    
+# Production de question aléatoires
+def randomQuestion(questions):
+    quiz = questions 
+    data = []
+    for i in range(0,10): 
+        item = random.choice(quiz)
+        quiz.remove(item)
+        data.append(item)
+    return data
 
+# Main     
+allQuestions = exportCSV('quiz.csv')   
+questions = randomQuestion(allQuestions)
+        
+for row in questions:
+    print(f"\n{row[0]}\n A.{row[1]}\n B.{row[2]}\n C.{row[3]}\n D.{row[4]}\n")
+    
+    correct_choice = False
+    
+    while not correct_choice:
+        response = input("Type A, B, C, or D\n")
+        if response.upper() in ['A', 'B', 'C', 'D']:
+            checkResponse(row[options[response.upper()]], row[5])
+            correct_choice = True
+        
+    count += 1
 
-# Programme principale
-with open('quiz.csv', 'r') as file:
-    reader = csv.reader(file)
-
-    for row in reader:
-        if count == 0:
-            count += 1
-        else:
-            print(f"\n{row[0]}\n A.{row[1]}\n B.{row[2]}\n C.{row[3]}\n D.{row[4]}\n")
-            correct_choice = False
-            
-            while not correct_choice:
-                response = input("Type A, B, C, or D\n")
-                if response.upper() in ['A', 'B', 'C', 'D']:
-                    checkResponse(row[options[response.upper()]], row[5])
-                    correct_choice = True
-                
-            count += 1
-
-    print('Merci!')
+showScore()
+print('Merci!')
